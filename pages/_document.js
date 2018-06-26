4fe57f930a6218abb73ba77bcb27f3f51dd73a5d
PHUA XUE YONG _ <xyphua.2014@smu.edu.sg>
2018-06-26 16:23:09 +0800

    first commit

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <style>{"body { margin: 0 } /* custom! */"}</style>
+          <meta
+            content="initial-scale=1.0, width=device-width, maximum-scale=1"
+            key="viewport"
+            name="viewport"
+          />
+          <link
+            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.1/semantic.min.css"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
