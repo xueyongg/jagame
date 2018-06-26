e1378bd089a6d56365d2453a868bb573d38d3f79
PHUA XUE YONG _ <xyphua.2014@smu.edu.sg>
2018-06-26 16:42:46 +0800

    updated index and package.json

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,4 @@
 import React, { Component } from "react";
-import {
-  Modal,
-  Header,
-  Button,
-  List,
-  Icon,
-  Input,
-  Menu,
-  Segment,
-  Image
-} from "semantic-ui-react";
 
 export default class Index extends Component {
   state = {};
