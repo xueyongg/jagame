21f6af795d817b12044cb7eab478bf83ceb1b10d
PHUA XUE YONG _ <xyphua.2014@smu.edu.sg>
2018-06-28 13:51:44 +0800

    added in style for header

--- pages/components/header.js
+++ pages/components/header.js
@@ -12,12 +12,32 @@ export default class PageHeader extends Component {
 
   render() {
     const { activeItem } = this.state;
+    let menuStyle = {
+      border: "none",
+      borderRadius: 0,
+      boxShadow: "none",
+      marginBottom: "1em",
+      marginTop: "1em",
+      transition: "box-shadow 0.5s ease, padding 0.5s ease",
+      position: "relative",
+      backgroundColor: "transparent",
+      left: "0",
+      right: "0",
+      paddingLeft: "5%",
+      paddingRight: "5%",
+      zIndex: 1,
+
+      fontWeight: 400,
+      fontSize: 16,
+      letterSpacing: ".15em"
+    };
+
     return (
-      <Segment basic>
+      <div>
         <Head>
           <title>Home</title>
         </Head>
-        <Menu secondary stackable>
+        <Menu secondary stackable style={menuStyle}>
           <Link href={{ pathname: "/index" }} passHref>
             <Menu.Item>
               <Image
@@ -39,18 +59,8 @@ export default class PageHeader extends Component {
               );
             }}
           </Context.Consumer>
-          <Menu.Menu position="right">
-            <Menu.Item>
-              <Input icon="search" placeholder="Search..." />
-            </Menu.Item>
-            <Menu.Item
-              name="logout"
-              active={activeItem === "logout"}
-              onClick={this.handleItemClick}
-            />
-          </Menu.Menu>
         </Menu>
-      </Segment>
+      </div>
     );
   }
 }
