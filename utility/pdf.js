dd17ce4023430d61f19b74e0ed4dec2d0522e30c
PHUA XUE YONG _ <xyphua.2014@smu.edu.sg>
2018-07-01 00:35:20 +0800

    added in configurations and setup required to get PDFMAKE library to work

--- utility/pdf.js
+++ utility/pdf.js
@@ -1,14 +1,16 @@
-// const pdfMake = require("pdfmake");
+const pdfMake = require("pdfmake/build/pdfmake.js");
+import font from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = font.pdfMake.vfs;
 
-export function createAndDownloadPDF (content){
-    let docDefinition ={
-        content: [
-            { text: 'This is a header', style: 'header' },
-            'No styling here, this is a standard paragraph',
-            { text: 'Another text', style: 'anotherStyle' },
-            { text: 'Multiple styles applied', style: [ 'header', 'anotherStyle' ] }
-          ],
-    }
+export function createAndDownloadPDF(content) {
+  let docDefinition = {
+    content: [
+      { text: "This is a header", style: "header" },
+      "No styling here, this is a standard paragraph",
+      { text: "Another text", style: "anotherStyle" },
+      { text: "Multiple styles applied", style: ["header", "anotherStyle"] }
+    ]
+  };
 
-    pdfMake.createPdf(docDefinition).download();
+  pdfMake.createPdf(docDefinition).download();
 }
